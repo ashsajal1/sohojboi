264e427cb18ad5f9079c340eebe99bc513c1a440
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 16:38:28 +0600

    feat: show dynamic ans

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -12,11 +12,17 @@ interface Params {
 
 export default async function Question({ params }: Params) {
     let question = null;
+    let answers = null;
     if (isValidObjectId(params.id)) {
         try {
             question = await prisma.question.findUnique({
                 where: {
                     id: params.id,
+                },
+            });
+            answers = await prisma.answer.findMany({
+                where: {
+                    questionId: params.id,
                 },
             });
         } catch (error) {
@@ -44,22 +50,47 @@ export default async function Question({ params }: Params) {
                 <CardFooter className="flex flex-col items-start">
                     <h2 className="mb-4">Others answers :</h2>
 
-                    <div className="flex">
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>John doe</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                This is others answer
-                            </CardContent>
-
-                            <CardFooter>
-                                <Button variant="outline">Upvote</Button>
-                            </CardFooter>
-                        </Card>
-                    </div>
+                    <Answers answers={answers} />
                 </CardFooter>
             </Card>
         </div>
     )
+}
+
+
+interface AnswersParams {
+    id: string;
+    userId: string;
+    questionId: string;
+    answerId: string;
+    answer: string;
+    upvoteCount: number;
+}
+
+interface AnswersProps {
+    answers: AnswersParams[];
+}
+const Answers = (answers: AnswersProps) => {
+    if (answers.answers.length === 0) {
+        return <h2 className="font-bold text-xl text-center text-muted-foreground">
+            Answers is empty!
+        </h2>
+    }
+
+    return <div className="flex">
+        {answers.answers.map(answer => (
+            <Card key={answer.id}>
+                <CardHeader>
+                    <CardTitle>John doe</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    This is others answer
+                </CardContent>
+
+                <CardFooter>
+                    <Button variant="outline">Upvote</Button>
+                </CardFooter>
+            </Card>
+        ))}
+    </div>
 }
