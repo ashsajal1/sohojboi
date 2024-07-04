cfab0ee33fc51bf98a93b258b36c5ce79120dc82
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 19:32:35 +0600

    feat: add optimistic upvote

--- src/app/question/[id]/upvote.tsx
+++ src/app/question/[id]/upvote.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { handleUpvote } from "./action";
+import { useOptimistic, useTransition } from "react";
 
 interface AnswersParams {
     id: string;
@@ -9,7 +10,22 @@ interface AnswersParams {
 }
 
 export default function Upvote({ id, upvoteCount }: AnswersParams) {
+    const [optimisticUpvotes, addOptimisticUpvote] = useOptimistic(
+        { upvoteCount, upvoting: false },
+        (state, newUpvoteCount: number) => ({
+            ...state,
+            upvoteCount: newUpvoteCount,
+            upvoting: true
+        })
+
+    )
+    // let [isPending, startTransition] = useTransition();
     return (
-        <Button onClick={() => handleUpvote(id, upvoteCount)} variant="outline">{upvoteCount} Upvote</Button>
+        // <Button onClick={() => startTransition(() => handleUpvote(id, upvoteCount))} variant="outline">{upvoteCount} {isPending ? 'Upvoting' : 'Upvote'}</Button>
+        <Button onClick={async () => {
+            addOptimisticUpvote(optimisticUpvotes.upvoteCount + 1);
+            await handleUpvote(id, upvoteCount)
+
+        }} variant="outline">{upvoteCount} {optimisticUpvotes.upvoting ? 'Upvoting' : 'Upvote'}</Button>
     )
 }
