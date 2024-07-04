474bbbc54acfb4dd67b28b7e850044889dd58cce
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 20:05:50 +0600

    feat: add url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma"
+import Link from "next/link";
 
 export default async function Home() {
   const questions = await prisma.question.findMany();
-  console.log(questions)
+  // console.log(questions)
   return (
     <div className="p-4">
       {questions.map(question => (
@@ -14,7 +15,7 @@ export default async function Home() {
           </CardHeader>
 
           <CardFooter className="flex items-center gap-3">
-            <Button>Answer</Button>
+            <Link href={`/question/${question.id}`}><Button>Answer</Button></Link>
             <Button variant={'outline'}>Upvote</Button>
           </CardFooter>
         </Card>
--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma"
+import Link from "next/link";
 
 export default async function Question() {
   const questions = await prisma.question.findMany();
@@ -14,7 +15,7 @@ export default async function Question() {
           </CardHeader>
 
           <CardFooter className="flex items-center gap-3">
-            <Button>Answer</Button>
+          <Link href={`/question/${question.id}`}><Button>Answer</Button></Link>
             <Button variant={'outline'}>Upvote</Button>
           </CardFooter>
         </Card>
