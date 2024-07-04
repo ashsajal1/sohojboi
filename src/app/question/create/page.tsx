4086522e356217ec801ce5297fa79436028b2cc7
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 19:55:11 +0600

    feat: create qustion page

--- src/app/question/create/page.tsx
+++ src/app/question/create/page.tsx
@@ -1,5 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Create() {
+  const createQuestion = async (formData: FormData) => {
+    "use server"
+    const title = formData.get("title")
+    const description = formData.get("description")
+    console.log(title, description)
+  }
   return (
-    <div>Create</div>
+    <div className="p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create question</CardTitle>
+        </CardHeader>
+        <form action={createQuestion}>
+          <CardContent>
+            <Input name="title" placeholder="Enter title..." />
+            <Textarea name="description" rows={12} placeholder="Enter description..." className="mt-3" />
+
+          </CardContent>
+          <CardFooter>
+            <Button>Submit</Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
   )
 }
