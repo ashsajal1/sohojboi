0cab13194422cdd14bcb2de22ba635b8dda9170a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 20:31:40 +0600

    style: flex layout

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -5,7 +5,7 @@ export default async function Question() {
     const questions = await prisma.question.findMany();
     // console.log(questions)
     return (
-        <div className="p-4">
+        <div className="p-4 flex flex-col gap-2">
             {questions.map(question => (
                 <QuestionCard key={question.id} question={question} />
             ))}
