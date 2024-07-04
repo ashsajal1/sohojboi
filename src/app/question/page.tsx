165301b91759b86e338f1636553dc420bec10136
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-04 19:56:11 +0600

    feat: create question page

---
+++ src/app/question/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import prisma from "@/lib/prisma"
+
+export default async function Question() {
+  const questions = await prisma.question.findMany();
+  console.log(questions)
+  return (
+    <div className="p-4">
+      {questions.map(question => (
+        <Card key={question.id}>
+          <CardHeader>
+          <CardTitle>{question.question}</CardTitle>
+          </CardHeader>
+
+          <CardFooter className="flex items-center gap-3">
+            <Button>Asnwer</Button>
+            <Button variant={'outline'}>Upvote</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  )
+}
