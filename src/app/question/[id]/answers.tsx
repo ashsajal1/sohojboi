dd37b0428f399dc242f1e9d3ae7d3365080897f2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-05 15:50:32 +0600

    ✨feature: show profile image

--- src/app/question/[id]/answers.tsx
+++ src/app/question/[id]/answers.tsx
@@ -1,22 +1,15 @@
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
 import Upvote from "./upvote";
-
-interface AnswersParams {
-    id: string;
-    userId: string;
-    questionId: string;
-    answer: string;
-    upvoteCount: number;
-    userFullName: string;
-}
+import { clerkClient } from "@clerk/nextjs/server";
+import { type Answer } from "@prisma/client";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
 
 interface AnswersProps {
-    answers: AnswersParams[];
+    answers: Answer[];
 }
-export const Answers = (answers: AnswersProps) => {
+export const Answers = async (answers: AnswersProps) => {
 
     if (answers.answers.length === 0) {
         return <h2 className="font-bold text-xl text-center text-muted-foreground">
@@ -27,18 +20,32 @@ export const Answers = (answers: AnswersProps) => {
     return <div className="flex flex-col gap-2 w-full">
         <Badge variant={'secondary'} className="mb-4">Community answers :</Badge>
         {answers.answers.map(answer => (
-            <Card key={answer.id}>
-                <CardHeader>
-                    <CardTitle>{answer.userFullName}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    {answer.answer}
-                </CardContent>
-
-                <CardFooter>
-                    <Upvote id={answer.id} upvoteCount={answer.upvoteCount} />
-                </CardFooter>
-            </Card>
+            <Answer key={answer.id} answer={answer} />
         ))}
     </div>
+}
+
+const Answer = async ({ answer }: { answer: Answer }) => {
+    let user;
+    let profileImageSrc;
+    try {
+        user = await clerkClient().users.getUser(answer.userId);
+        profileImageSrc = user.imageUrl;
+    } catch (error: any) {
+        // throw new Error(error.message)
+    }
+
+    return <Card>
+        <CardHeader className={cn("flex items-center gap-2")}>
+            {profileImageSrc && <Image className="rounded-full" width={30} height={30} src={profileImageSrc} alt={"Profile image"} />}
+            <CardTitle>{answer.userFullName}</CardTitle>
+        </CardHeader>
+        <CardContent>
+            {answer.answer}
+        </CardContent>
+
+        <CardFooter>
+            <Upvote id={answer.id} upvoteCount={answer.upvoteCount} />
+        </CardFooter>
+    </Card>
 }
