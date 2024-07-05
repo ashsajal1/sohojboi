80e66b3e3ed0ef2629008a084fbeb6d2616a3512
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-05 11:54:56 +0600

    ✨feature: handle empty questions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import prisma from "@/lib/prisma"
 import QuestionCard from "./question/question-card";
+import { Card, CardTitle } from "@/components/ui/card";
 
 export default async function Home() {
   const questions = await prisma.question.findMany();
   // console.log(questions)
+  if(questions.length === 0) {
+    return <Card className="p-4 m-12">
+        <CardTitle>Question is empty!</CardTitle>
+    </Card>
+}
   return (
     <div className="p-4 flex flex-col gap-2">
       {questions.map(question => (
