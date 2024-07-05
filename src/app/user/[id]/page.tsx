e60d175671987d16b697440acdc112a6772df58c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-05 14:31:26 +0600

    ✨feature: show stats

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,17 +1,38 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import prisma from "@/lib/prisma";
+import { Badge } from "@/components/ui/badge";
+import { Label } from "@/components/ui/label";
 export default async function Page({ params }: { params: { id: string } }) {
     const user = await clerkClient().users.getUser(params.id)
     // console.log(user)
+    const questions = await prisma.question.findMany({
+        where: {
+            userId: user.id
+        }
+    })
+    const answers = await prisma.answer.findMany({
+        where: {
+            userId: user.id
+        }
+    })
     return (
         <Card className="mt-2">
             <CardHeader>
                 <CardTitle>Profile details of {user.fullName}</CardTitle>
             </CardHeader>
-            <CardContent>
-                <Image className="rounded-ull" width={100} height={100} alt="user image" src={user.imageUrl} />
-                <p>{user.fullName}</p>
+            <CardContent className="flex items-center justify-between">
+                <div>
+                    <Image className="rounded-ull" width={100} height={100} alt="user image" src={user.imageUrl} />
+                    <p>{user.fullName}</p>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                    <Label>User stats : </Label>
+                    <Badge>Total questions : {questions.length}</Badge>
+                    <Badge>Total answers : {answers.length}</Badge>
+                </div>
             </CardContent>
         </Card>
     )
