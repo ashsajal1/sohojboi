98adba58634177873fc1c45e8cc17a8191d82ff4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-05 14:55:18 +0600

    ✨feature: create not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div className="grid place-items-center p-12">
+            <Card className="grid place-items-center p-6">
+                <CardHeader>
+                    <h1 className="font-bold text-3xl font-mono text-center">404</h1>
+                    <p>The content you are looking for is not found!</p>
+                </CardHeader>
+                <CardContent className="flex gap-2 items-center">
+                    <Link href={'/'}>
+                        <Button>
+                            Home
+                        </Button>
+                    </Link>
+                    <Button variant={'outline'} onClick={() => location.reload()}>
+                        Back
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
