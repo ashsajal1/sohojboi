28b84265c7ffb75027b808f4f1af0e95af64f697
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-06 23:41:43 +0600

    🪄 style: gap between notificatiion card

--- src/app/(private)/notifications/page.tsx
+++ src/app/(private)/notifications/page.tsx
@@ -24,7 +24,7 @@ export default async function page() {
         }
     }
     return (
-        <div>
+        <div className='flex flex-col gap-2'>
             {notifications?.map(notification => (
                 <Card key={notification.id}>
                     <CardHeader>
