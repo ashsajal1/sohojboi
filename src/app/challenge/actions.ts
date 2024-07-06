457e5068c359967194550653eaf0107b70af946e
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-06 20:04:22 +0600

    🐞 fix: notification schema

--- src/app/challenge/actions.ts
+++ src/app/challenge/actions.ts
@@ -1,18 +1,31 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
 
-export const createCompetition = async (challangeeId: string, challangerId: string, quizId:string, challengerScore: number ) => {
-    const competition = await prisma.competition.create({
-        data: {
-          title: `${challangerId} vs ${challangeeId}`,
-          description: `A competition`,
-          quizId: quizId,
-          challengerId: challangerId,
-          challengeeId: challangeeId,
-          challengerScore: challengerScore,
-          status: 'pending'
-        }
-      });
-      console.log(competition)
-}
+export const createCompetition = async (
+  challangeeId: string,
+  challangerId: string,
+  quizId: string,
+  challengerScore: number
+) => {
+  const competition = await prisma.competition.create({
+    data: {
+      title: `${challangerId} vs ${challangeeId}`,
+      description: `A competition`,
+      quizId: quizId,
+      challengerId: challangerId,
+      challengeeId: challangeeId,
+      challengerScore: challengerScore,
+      status: "pending",
+    },
+  });
+  // console.log(competition)
+  const notif = await prisma.notification.create({
+    data: {
+      userId: challangeeId,
+      message: `${challangerId} challenged you in a quiz!`
+    }
+  })
+
+  console.log(notif)
+};
