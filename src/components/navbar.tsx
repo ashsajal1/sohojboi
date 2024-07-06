b02f0b39ed256f8ad18c3cfd12da3b1bb5f6249a
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-06 20:20:19 +0600

    ✨feature: show notification in bell icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,13 +2,37 @@ import Link from "next/link";
 import { Button } from "./ui/button";
 import { ModeToggle } from "./mode-toggle";
 import { UserButton, SignedIn, SignedOut } from '@clerk/nextjs'
+import { BellIcon } from "@radix-ui/react-icons";
+import { Badge } from "./ui/badge";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { Notification } from "@prisma/client";
 
-export default function Navbar() {
+export default async function Navbar() {
+    const userId = auth().userId;
+    let notifications: null | Notification[] = null;
+    if (userId) {
+        try {
+            notifications = await prisma.notification.findMany({
+                where: {
+                    userId: userId as string
+                }
+            })
+        } catch (error) {
+            notifications = null
+        }
+    }
     return (
         <nav className="flex items-center justify-between p-4 border-b">
             <Link href={'/'} className="font-bold text-xl">SohojBoi</Link>
             <div className="flex items-center gap-2">
                 <ModeToggle />
+                <SignedIn>
+                    <Button className="relative mx-2" size={'icon'} variant={'outline'}>
+                        <Badge className="absolute -right-3 -top-3" variant={'destructive'}>{notifications?.length}</Badge>
+                        <BellIcon />
+                    </Button>
+                </SignedIn>
                 <Link href={'/question'}>
                     <Button>Question</Button>
                 </Link>
