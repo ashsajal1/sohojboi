01147ba05f8a735494d024c6085b1ded1ca39aeb
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-06 15:04:47 +0600

    ✨feature: accept and play challange quiz

--- src/app/challange/accept/accept-btn.tsx
+++ src/app/challange/accept/accept-btn.tsx
@@ -5,10 +5,7 @@ import React from 'react'
 
 export default function AcceptBtn({competitionId}:{competitionId: string}) {
     const handleAccept = async () => {
-        // await prisma.competition.update({
-        //     where: { id: competitionId },
-        //     data: { status: "accepted" }
-        // });
+       
 
         // Redirect to the competition play page or reload
         window.location.href = `/challange/result?competitionId=${competitionId}`;
--- src/app/challange/accept/page.tsx
+++ src/app/challange/accept/page.tsx
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs/server';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import AcceptBtn from './accept-btn';
+import Challange from './challange-quiz';
 
 export default async function AcceptChallengePage({ searchParams }: { searchParams: any }) {
     const { userId } = auth();
@@ -30,11 +31,53 @@ export default async function AcceptChallengePage({ searchParams }: { searchPara
         }
     });
 
+    const quizzes = await prisma.quiz.findMany({
+        orderBy: {
+          createdAt: 'desc'
+        },
+        where: {
+            id: competition?.quizId
+        },
+        include: {
+          questions: {
+            include: {
+              options: true
+            }
+          },
+        }
+      })
+    
+      const quizQuestions = quizzes.map((quiz) => ({
+        id: quiz.id,
+        title: quiz.title,
+        questions: quiz.questions.map((question) => ({
+          id: question.id,
+          text: question.content,
+          options: question.options.map((option) => ({
+            id: option.id,
+            text: option.content,
+            isCorrect: option.isCorrect
+          }))
+        }))
+      })).map(quesitons => quesitons.questions)[0];
+
+    // const quizQuestions = quizzes.map((quiz) => ({
+    //     id: quiz.id,
+    //     title: quiz.title,
+    //     questions: quiz.questions.map((question) => ({
+    //       id: question.id,
+    //       text: question.content,
+    //       options: question.options.map((option) => ({
+    //         id: option.id,
+    //         text: option.content,
+    //         isCorrect: option.isCorrect
+    //       }))
+    //     }))
+    //   })).map(quesitons => quesitons.questions)[0];
+
     if (!competition) {
         return <div>Competition not found.</div>;
     }
-
-    
 
     return (
         <div>
@@ -45,6 +88,7 @@ export default async function AcceptChallengePage({ searchParams }: { searchPara
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
+                    <Challange competitionId={competition.id} quizQuestions={quizQuestions} />
                     <AcceptBtn competitionId={competitionId} />
                 </CardContent>
             </Card>
