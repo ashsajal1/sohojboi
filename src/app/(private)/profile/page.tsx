f21955b52f6df4d6249ef6cae1ac592d7908e604
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-07 15:13:24 +0600

    ✨feature: show winner status

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -11,7 +11,9 @@ import {
 } from "@/components/ui/tabs"
 import QuestionCard from "@/app/question/question-card";
 import { Answers } from "@/app/question/[id]/answers";
-import { currentUser } from '@clerk/nextjs/server';
+import { clerkClient, currentUser } from '@clerk/nextjs/server';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default async function Page() {
     const user = await currentUser();
@@ -25,6 +27,16 @@ export default async function Page() {
         where: {
             userId: user?.id
         }
+    })
+
+    const challenges = await prisma.competition.findMany({
+        where: {
+            OR: [
+                { challengerId: user?.id },
+                { challengeeId: user?.id },
+            ],
+            status: "completed"
+        },
     })
     return (
         <>
@@ -47,9 +59,10 @@ export default async function Page() {
             </Card>
 
             <Tabs defaultValue="questions">
-                <TabsList className="grid w-full grid-cols-2">
+                <TabsList className="grid w-full grid-cols-3">
                     <TabsTrigger value="questions">Asked Questions</TabsTrigger>
                     <TabsTrigger value="answers">Answers</TabsTrigger>
+                    <TabsTrigger value="challenges">Challenges</TabsTrigger>
                 </TabsList>
 
                 <TabsContent value="questions">
@@ -60,7 +73,61 @@ export default async function Page() {
                 <TabsContent value="answers">
                     <Answers answers={answers} />
                 </TabsContent>
+                <TabsContent value="challenges">
+                    {challenges.map(challenge => (
+                        <Card className="mb-2" key={challenge.id}>
+                            <CardHeader>
+                                <Winner
+                                    challengerId={challenge.challengerId}
+                                    challengeeId={challenge.challengeeId}
+                                    userId={user?.id || ''}
+                                    challengerScore={challenge.challengerScore}
+                                    challengeeScore={challenge.challengeeScore || 0}
+                                />
+                                <CardTitle>
+                                    Competition between: {challenge.title}
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <p><strong>Challenger Score:</strong> {challenge.challengerScore}</p>
+                                <p><strong>Opponent&apos;s Score:</strong> {challenge.challengeeScore}</p>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </TabsContent>
             </Tabs>
         </>
     )
+}
+
+const Winner = async ({ challengerId, challengeeId, userId, challengerScore, challengeeScore }: { challengerId: string, challengeeId: string, userId: string, challengerScore: number, challengeeScore: number }) => {
+
+    let resultText = '';
+    let badgeVariant: 'secondary' | 'destructive' = 'secondary';
+
+    const challenger = await clerkClient.users.getUser(challengerId);
+    const challengee = await clerkClient.users.getUser(challengeeId);
+
+    if (challengerScore === challengeeScore) {
+        resultText = 'It\'s a draw';
+    } else {
+        const winnerId = challengerScore > challengeeScore ? challengerId : challengeeId;
+        const winnerName = winnerId === challengerId ? challenger.fullName : challengee.fullName;
+        const loserName = winnerId === challengerId ? challengee.fullName : challenger.fullName;
+        const userWon = winnerId === userId;
+
+        if (userWon) {
+            resultText = `${winnerName} won against ${loserName}`;
+            badgeVariant = 'secondary';
+        } else {
+            resultText = `${loserName} lost against ${winnerName}`;
+            badgeVariant = 'destructive';
+        }
+    }
+
+    return (
+        <Badge variant={badgeVariant}>
+            {resultText}
+        </Badge>
+    );
 }
