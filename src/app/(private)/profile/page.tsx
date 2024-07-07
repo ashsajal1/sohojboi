a349013d0a8919e540c0f817055fe8b8f6a6c5e7
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-07 15:47:43 +0600

    ✨feature: update clerkClient

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -137,8 +137,8 @@ const Winner = async ({ challengerId, challengeeId, userId, challengerScore, cha
 
 
 const Names = async ({ challenge }: { challenge: Competition }) => {
-    const challenger = (await clerkClient.users.getUser(challenge.challengerId)).lastName;
-    const challengee = (await clerkClient.users.getUser(challenge.challengeeId)).lastName;
+    const challenger = (await clerkClient().users.getUser(challenge.challengerId)).lastName;
+    const challengee = (await clerkClient().users.getUser(challenge.challengeeId)).lastName;
     return <>
         <p><strong>{challenger}&apos;s Score:</strong> {challenge.challengerScore}</p>
         <p><strong>{challengee}&apos;s Score:</strong> {challenge.challengeeScore}</p>
