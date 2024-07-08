52ec772c06bf05f69ec450dcae077961f260d588
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-08 17:10:14 +0600

    ✨feature: show user profile name and image in question card

--- src/app/question/question-card.tsx
+++ src/app/question/question-card.tsx
@@ -3,9 +3,10 @@ import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import UpvoteBtn from "./upvote-btn";
 import { formatDate } from "@/lib/date-format";
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { Question } from "@prisma/client";
 import prisma from "@/lib/prisma";
+import Image from "next/image";
 
 interface QuestionProps {
     question: Question;
@@ -13,6 +14,8 @@ interface QuestionProps {
 export default async function QuestionCard({ question }: QuestionProps) {
     const actorId = await auth().userId;
     let isUpvotedQuestion;
+    let profileImageSrc;
+    let questionUser;
 
     isUpvotedQuestion = await prisma.upvote.findUnique({
         where: {
@@ -24,6 +27,13 @@ export default async function QuestionCard({ question }: QuestionProps) {
     })
     isUpvotedQuestion = !!isUpvotedQuestion
 
+    try {
+        questionUser = await clerkClient().users.getUser(question.userId);
+        profileImageSrc = questionUser.imageUrl;
+    } catch (error: any) {
+        // throw new Error(error.message)
+    }
+
     return (
         <Card>
             <CardHeader>
@@ -34,9 +44,22 @@ export default async function QuestionCard({ question }: QuestionProps) {
                 </div>
             </CardHeader>
 
-            <CardFooter className="flex items-center gap-3">
-                <Link href={`/question/${question.id}`}><Button>Answer</Button></Link>
-                <UpvoteBtn isUpvotedQuestion={isUpvotedQuestion} question={question} actorId={actorId || ''} />
+            <CardFooter className="flex items-center justify-between">
+
+                <Link className="flex items-center gap-2" href={`/profile?id=${question.userId}`}>
+                    {profileImageSrc && <Image className="rounded-full" width={30} height={30} src={profileImageSrc} alt={"Profile image"} />}
+                    <p className="flex flex-col">
+                        {question.userFullName}
+                        <span className="text-sm text-muted-foreground">Asked {formatDate(question.createdAt)}</span>
+                    </p>
+                </Link>
+
+                <div className="flex items-center gap-2">
+                    <Link href={`/question/${question.id}`}>
+                        <Button>Answer</Button>
+                    </Link>
+                    <UpvoteBtn isUpvotedQuestion={isUpvotedQuestion} question={question} actorId={actorId || ''} />
+                </div>
             </CardFooter>
         </Card>
     )
