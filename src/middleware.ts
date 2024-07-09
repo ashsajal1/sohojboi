035a8654774633fbd4991a82f72fe187604d7a3c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-09 11:46:30 +0600

    ✨feature: update middleware to latest

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,4 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
   '/question/create(.*)',
