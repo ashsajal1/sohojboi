5cdb9ff439fae40502694d544624b47d981b25a5
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-09 13:09:23 +0600

    ✨feature: redirect to question after deletion

--- src/app/question/edit/[id]/actions.ts
+++ src/app/question/edit/[id]/actions.ts
@@ -1,13 +1,16 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export const deleteQuestion = async (questionId: string) => {
   try {
-    await prisma.question.delete({
+    const deletedQuestion = await prisma.question.delete({
       where: {
         id: questionId,
       },
     });
+
+    if(deletedQuestion) redirect('/question')
   } catch (error) {}
 };
