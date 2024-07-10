c261087e350dd6c525b3962f27ee819e23e88599
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-10 16:26:35 +0600

    ✨feature: add defaut.tsx

---
+++ src/app/@questions/default.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function Default() {
+    return <>
+    <Card>
+        <CardContent>
+            Questions slot is emprty!
+        </CardContent>
+    </Card>
+    </>
+}
