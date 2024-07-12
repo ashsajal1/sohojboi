44f4cdd32df2c9b324da5d0e4d9ddf5cd5de2aba
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-12 19:02:55 +0600

    ✨feature: filter by question content

--- src/app/(admin)/admin/panel/@questions/data-table.tsx
+++ src/app/(admin)/admin/panel/@questions/data-table.tsx
@@ -74,10 +74,10 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("emailAddresses")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter question..."
+          value={(table.getColumn("content")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("emailAddresses")?.setFilterValue(event.target.value)
+            table.getColumn("content")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
