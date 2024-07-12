13860ae780fd2118f88239d4bcc07a80c2577f6b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-12 21:37:06 +0600

    ✨feature: add header

--- src/app/(admin)/admin/panel/page.tsx
+++ src/app/(admin)/admin/panel/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className="container mx-auto">page</div>
+    <div className="container mx-auto">
+      <h1 className='font-bold text-lg'>Users and Challenge questions table</h1>
+    </div>
   )
 }
