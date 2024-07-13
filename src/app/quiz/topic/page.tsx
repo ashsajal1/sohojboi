63bdaa7ed55ca15cf7e7d68f10dce208dff1d460
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-13 11:33:06 +0600

    ✨feature: create topic page

---
+++ src/app/quiz/topic/page.tsx
@@ -0,0 +1,23 @@
+import { Badge } from "@/components/ui/badge"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import prisma from "@/lib/prisma"
+
+export default async function TopicPage() {
+    const topics = await prisma.topic.findMany()
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    <CardHeader>
+                        <CardTitle>Here is the list of topics</CardTitle>
+                    </CardHeader>
+                    <div className="flex items-center gap-2 flex-wrap">
+                        {topics.map(topic => (
+                            <Badge key={topic.id}>{topic.name}</Badge>
+                        ))}
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
