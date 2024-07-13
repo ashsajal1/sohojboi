e22a27cc06408aea4f10ba3accf7eace4f0b57c1
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-13 20:25:43 +0600

    ✨feature: add seo tag in write question page

--- src/app/question/create/page.tsx
+++ src/app/question/create/page.tsx
@@ -1,6 +1,14 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import CreateForm from "./create-form";
 import prisma from "@/lib/prisma";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Sohojboi: Write your question",
+  description: "Sohojboi : Write your question",
+};
+
 
 export default async function Create() {
   const topics = await prisma.topic.findMany()
