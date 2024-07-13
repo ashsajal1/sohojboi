d77fa09a0eeecc012a77d06859ac0284fe698f13
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-13 10:17:43 +0600

    🐞 fix: violate the required relation 'AnswerOptionToChallengeQuestion'

--- src/app/(admin)/admin/panel/@questions/actions.ts
+++ src/app/(admin)/admin/panel/@questions/actions.ts
@@ -1,11 +1,20 @@
-"use server"
+"use server";
 
-import prisma from "@/lib/prisma"
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export const deleteQuestion = async (id: string) => {
+  try {
+    await prisma.answerOption.deleteMany({
+      where: { challengeQuestionId: id },
+    });
+
     await prisma.challengeQuestion.delete({
-        where: {
-            id: id
-        }
-    })
-}
+      where: { id: id },
+    });
+
+    revalidatePath("/");
+  } catch (err: any) {
+    throw new Error("Cannot delete question!");
+  }
+};
