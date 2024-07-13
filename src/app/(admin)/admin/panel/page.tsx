e3fbb02114f0aee60739c1ebfb6630dc7a9572ed
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-13 09:44:48 +0600

    ✨feature: check role before entering admin panel

--- src/app/(admin)/admin/panel/page.tsx
+++ src/app/(admin)/admin/panel/page.tsx
@@ -1,6 +1,11 @@
+import { checkRole } from '@/lib/roles'
 import React from 'react'
 
 export default function page() {
+  const isAdmin = checkRole("admin");
+  if (!isAdmin) {
+    throw new Error("Unauthorized access!")
+  }
   return (
     <div className="container mx-auto">
       <h1 className='font-bold text-lg'>Users and Challenge questions table</h1>
