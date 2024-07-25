e45af03052442ade2711fe710e73f8248e6e1440
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-25 12:10:45 +0600

    ✨feature: wrap actions inside try catch

--- src/app/(private)/profile/edit/actions.ts
+++ src/app/(private)/profile/edit/actions.ts
@@ -6,22 +6,26 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export const createOrUpdateProfile = async (data: EditFormSchema) => {
-  const user = await currentUser();
-  const updatedProfile = await prisma.profile.upsert({
-    where: {
-      clerkUserId: user?.id,
-    },
-    update: {
-      bio: data.bio,
-    },
-    create: {
-      clerkUserId: user?.id,
-      bio: data.bio,
-      grade: parseInt(data.grade),
-      address: data.address,
-    },
-  });
+  try {
+    const user = await currentUser();
+    const updatedProfile = await prisma.profile.upsert({
+      where: {
+        clerkUserId: user?.id,
+      },
+      update: {
+        bio: data.bio,
+      },
+      create: {
+        clerkUserId: user?.id,
+        bio: data.bio,
+        grade: parseInt(data.grade),
+        address: data.address,
+      },
+    });
 
-  console.log(updatedProfile);
-  redirect("/profile")
+    console.log(updatedProfile);
+    redirect("/profile");
+  } catch (error) {
+    throw error;
+  }
 };
