39722d41a11259e4b6961b1bbef5bc65caf3e0f0
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-26 16:00:16 +0600

    ✨feature: add sanitize

--- src/app/article/[id]/content.tsx
+++ src/app/article/[id]/content.tsx
@@ -1,9 +1,11 @@
 "use client"
 import MarkdownPreview from '@uiw/react-markdown-preview';
+import rehypeSanitize from "rehype-sanitize";
+const rehypePlugins = [rehypeSanitize];
 
 export default function Content({ content }: { content: string }) {
     return (
-        <MarkdownPreview className='p-4' source={content}>
+        <MarkdownPreview className='p-4' rehypePlugins={rehypePlugins} source={content}>
 
         </MarkdownPreview>
     )
