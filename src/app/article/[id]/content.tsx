a20cc379d55ecc9a3f0160e8a00acffe5bce2cc7
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-26 16:19:27 +0600

    ✨feature: set article content theme as next theme

--- src/app/article/[id]/content.tsx
+++ src/app/article/[id]/content.tsx
@@ -2,12 +2,14 @@
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import rehypeSanitize from "rehype-sanitize";
 const rehypePlugins = [rehypeSanitize];
+import { useTheme } from "next-themes"
 
 export default function Content({ content }: { content: string }) {
+    const { theme } = useTheme();
     return (
         <MarkdownPreview
             wrapperElement={{
-                "data-color-mode": "light"
+                "data-color-mode": theme as "light" | "dark" | undefined
             }}
             className='p-4'
             rehypePlugins={rehypePlugins}
