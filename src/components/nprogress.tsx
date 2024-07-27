91492cbfa3733315fdfcb3a73b4b8de361fc436d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-27 14:28:34 +0600

    ✨feature: add nprogress

---
+++ src/components/nprogress.tsx
@@ -0,0 +1,19 @@
+// Create a Providers component to wrap your application with all the components requiring 'use client', such as next-nprogress-bar or your different contexts...
+'use client';
+
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+
+const Nprogress = () => {
+  return (
+    <>
+      <ProgressBar
+        height="2px"
+        color="#fffd00"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
+    </>
+  );
+};
+
+export default Nprogress;
