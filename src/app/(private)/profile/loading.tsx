50895d8972018c21981ce389d7a5b8bd0e720835
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-27 13:58:19 +0600

    ✨feature: add custom loading in profile page

---
+++ src/app/(private)/profile/loading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
+        <Skeleton className='w-full h-[250px]' />
+        <Skeleton className='w-full h-[50px]' />
+
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+        <Skeleton className='w-full h-[200px]' />
+    </div>
+  )
+}
