5bc3e31cf5dbfed1f73ecf928ecf6a18d5383495
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-27 13:55:26 +0600

    style: update skelton layout for sm screen

--- src/app/@questions/loading.tsx
+++ src/app/@questions/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function loading() {
   return (
-    <div className='md:grid grid-cols-2 gap-2'>
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
         <Skeleton className='w-full h-[200px]' />
         <Skeleton className='w-full h-[200px]' />
         <Skeleton className='w-full h-[200px]' />
