fb6f07df0b96d690aacd17d1498a6e2d88b10f35
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-07-31 10:20:40 +0600

    ✨feature: update pagination of question

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,7 +1,5 @@
 import prisma from "@/lib/prisma"
 import QuestionCard from "./question-card";
-import { Card, CardTitle } from "@/components/ui/card";
-
 import { Metadata } from "next";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -16,6 +14,7 @@ export default async function Page({ searchParams }: { searchParams: { page: str
     const page = parseInt(searchParams.page) || 1;
     const skipSize = (page - 1) * 10;
 
+    const totalQuestions = await prisma.question.count();
     const questions = await prisma.question.findMany({
         skip: skipSize,
         take: 10,
@@ -23,6 +22,8 @@ export default async function Page({ searchParams }: { searchParams: { page: str
             createdAt: 'desc'
         }
     });
+
+    const hasMoreQuestions = totalQuestions > skipSize + questions.length;
 
     return (
         <>
@@ -41,18 +42,22 @@ export default async function Page({ searchParams }: { searchParams: { page: str
 
             {questions.length === 0 && <h1 className='font-bold text-xl text-center p-12'>No questions Found!</h1>}
             <div className='mt-2 flex items-center justify-between gap-2'>
-                {page > 1 && <Link href={`/question?page=${page - 1}`}>
-                    <Button>
-                        <ArrowLeft className='h-4 w-4 mr-2' />
-                        Previous Page
-                    </Button>
-                </Link>}
-                <Link href={`/question?page=${page + 1}`}>
-                    <Button disabled={questions.length === 0} variant={'outline'}>
-                        Next Page
-                        <ArrowRight className='h-4 w-4 ml-2' />
-                    </Button>
-                </Link>
+                {page > 1 && (
+                    <Link href={`/question?page=${page - 1}`}>
+                        <Button variant={'outline'}>
+                            <ArrowLeft className='h-4 w-4 mr-2' />
+                            Previous Page
+                        </Button>
+                    </Link>
+                )}
+                {hasMoreQuestions && (
+                    <Link href={`/question?page=${page + 1}`} className='ml-auto w-full md:w-auto'>
+                        <Button className='w-full md:w-auto'>
+                            Next Page
+                            <ArrowRight className='h-4 w-4 ml-2' />
+                        </Button>
+                    </Link>
+                )}
             </div>
         </>
     )
