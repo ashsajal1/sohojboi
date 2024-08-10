9b0a210a8654652d99bd39a2abf75f4497e24fe8
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-10 10:47:59 +0600

    🦄 refactor: remove log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from "@/components/hero-section";
-import { logger } from "@/logger";
 
 export default async function Home() {
-  logger.info("hello world")
   return (
     <>
       <HeroSection />
