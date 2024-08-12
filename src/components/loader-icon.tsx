4e9ece06e373e365bae76b2de50d00ddb31f418d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-12 09:09:45 +0600

    ✨ feat: create loader icon

---
+++ src/components/loader-icon.tsx
@@ -0,0 +1,8 @@
+import { LoaderCircle } from 'lucide-react'
+import React from 'react'
+
+export default function LoaderIcon() {
+  return (
+    <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+  )
+}
