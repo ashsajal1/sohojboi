966f3ae42b937c294136359c0a4d5c8886302df7
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-12 09:25:15 +0600

    ✨ feat: add progress

--- src/app/(private)/notifications/mark-read-btn.tsx
+++ src/app/(private)/notifications/mark-read-btn.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { Badge } from '@/components/ui/badge'
-import React from 'react'
+import React, { useTransition } from 'react'
 import { handleMarkRead } from './actions';
+import { LoaderCircle } from 'lucide-react';
 
 export default function MarkReadBtn({
     notificationId, status
@@ -10,11 +11,21 @@ export default function MarkReadBtn({
     notificationId: string;
     status: boolean
 }) {
+
+    const [pending, startTransition] = useTransition();
     return (
         <>
             {(!status) &&
-                < Badge onClick={() => handleMarkRead(notificationId)
-                } className='cursor-pointer select-none' variant={'outline'} >
+                <Badge
+                    onClick={async () => {
+                        await startTransition(async () => {
+                            await handleMarkRead(notificationId)
+                        })
+                    }
+                    }
+                    className='cursor-pointer select-none' variant={'outline'} >
+                    {pending &&
+                        <LoaderCircle className="mr-2 h-2 w-2 animate-spin" />}
                     Mark as read
                 </Badge >}
         </>
