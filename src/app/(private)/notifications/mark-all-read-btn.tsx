9c2af176db29a157d59e72b57cafc2af9d03f41c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-12 08:38:06 +0600

    🐞 fix: #46

--- src/app/(private)/notifications/mark-all-read-btn.tsx
+++ src/app/(private)/notifications/mark-all-read-btn.tsx
@@ -4,14 +4,22 @@ import { Button } from "@/components/ui/button"
 import { readAllNotificaiton } from "./actions"
 import { CheckIcon } from "@radix-ui/react-icons"
 import { useAuth } from "@clerk/nextjs";
+import { useTransition } from "react";
+import { LoaderCircle } from "lucide-react";
 
 export default function MarkAllReadBtn() {
     const { isLoaded, userId } = useAuth();
+    const [pending, startTransition] = useTransition();
     return (
         <>
             {isLoaded && <div className="flex items-end justify-end">
-                <Button onClick={() => readAllNotificaiton(userId || '')} variant={"secondary"}>
-                    <CheckIcon className="mr-2" />
+                <Button disabled={pending} onClick={async () => {
+                    await startTransition(async () => {
+                        await readAllNotificaiton(userId!);
+                    })
+                }} variant={"secondary"}>
+                    {pending ?
+                        <LoaderCircle className="mr-2 h-4 w-4 animate-spin" /> : <CheckIcon className="mr-2" />}
                     Mark all as read
                 </Button>
             </div>}
