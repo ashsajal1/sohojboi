d0db8d70813c84cd9c1820431c808e312dcf189f
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-13 20:11:22 +0600

    ✨ feat: add edit dialog

--- src/app/article/[id]/comment-drop-down.tsx
+++ src/app/article/[id]/comment-drop-down.tsx
@@ -12,9 +12,11 @@ import {
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import { Ellipsis } from "lucide-react";
 import DeleteDialog from "./delete-dialog";
+import EditDialog from "./edit-dialog";
 
-export default function CommentDropDown({ commentId }: { commentId: string }) {
+export default function CommentDropDown({ commentId, commentText }: { commentId: string, commentText: string }) {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
 
   const handleDeleteClick = () => {
     setIsDeleteDialogOpen(true);
@@ -36,7 +38,7 @@ export default function CommentDropDown({ commentId }: { commentId: string }) {
       <DropdownMenuContent>
         <DropdownMenuGroup>
           <DropdownMenuLabel>Comment Options</DropdownMenuLabel>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setIsEditDialogOpen(true)}>
             <Pencil1Icon className="mr-1 h-4 w-4" />
             Edit
           </DropdownMenuItem>
@@ -50,6 +52,7 @@ export default function CommentDropDown({ commentId }: { commentId: string }) {
       </DropdownMenuContent>
     </DropdownMenu>
     <DeleteDialog open={isDeleteDialogOpen} onClose={handleDeleteDialogClose} />
+    <EditDialog value={commentText} open={isEditDialogOpen} onClose={() => setIsEditDialogOpen(false)} />
     </>
   );
 }
