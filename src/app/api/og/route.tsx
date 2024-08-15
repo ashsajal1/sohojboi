5c9ff7fa004d581bc6da51679e9b1b7bbf3533d9
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-15 09:31:30 +0600

    🌈 style: align items start

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -48,7 +48,7 @@ export async function GET(request: NextRequest) {
 
                 <div tw="flex items-center">
                     <img tw="rounded-full" src={profileImg!} width='40' height='40' />
-                    <div tw='flex flex-col items-center ml-4'>
+                    <div tw='flex flex-col items-start ml-4'>
                         <p tw='font-bold text-xl mb-0'>{authorName}</p>
                         <p tw='text-slate-600 mt-0'>{date}</p>
                     </div>
