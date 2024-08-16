7f2084bab398f8e293703a1c9eda807afe0e6621
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-16 10:41:42 +0600

    ✨ feat: show selecting

--- src/app/challenge/select.tsx
+++ src/app/challenge/select.tsx
@@ -5,15 +5,21 @@ import { User } from "@clerk/nextjs/server"
 import { Button } from "@/components/ui/button"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import Image from "next/image"
+import { useTransition } from "react"
+import LoaderIcon from "@/components/loader-icon"
 
 export default function Select({ users, userId }: { users: User[], userId: string }) {
     const searchParams = useSearchParams();
     const pathName = usePathname();
-    const { replace } = useRouter()
-    const handleSearchParms = (userId: string) => {
+    const { replace } = useRouter();
+    const [pending, startTransiton] = useTransition();
+
+    const handleSearchParms = async (userId: string) => {
         const params = new URLSearchParams(searchParams);
-        params.set('challengeeId', userId);
-        replace(`${pathName}?${params.toString()}`);
+        await startTransiton(async () => {
+            await params.set('challengeeId', userId);
+            await replace(`${pathName}?${params.toString()}`);
+        })
     }
 
     const filteredUsers = users.filter(user => user.id != userId)
@@ -26,7 +32,9 @@ export default function Select({ users, userId }: { users: User[], userId: strin
                         <Image className="rounded-full" width={30} height={30} src={user.imageUrl} alt={"Profile image"} />
                         <CardTitle>{user.firstName} {user.lastName}</CardTitle>
                     </div>
-                    <Button size="sm" variant="outline" onClick={() => handleSearchParms(user.id)}>Select</Button>
+                    <Button disabled={pending} size="sm" variant="outline" onClick={() => handleSearchParms(user.id)}>
+                        {pending? <><LoaderIcon />Selecting</>: 'Select'}
+                    </Button>
                 </Card>
             ))}
         </div>
