79dba8066539e50c175fb61014834e7ea3903e5d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-16 11:01:19 +0600

    🐞 fix: right loading place

--- src/app/challenge/select.tsx
+++ src/app/challenge/select.tsx
@@ -1,28 +1,41 @@
 "use client"
 
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { User } from "@clerk/nextjs/server"
-import { Button } from "@/components/ui/button"
-import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import Image from "next/image"
-import { useTransition } from "react"
-import LoaderIcon from "@/components/loader-icon"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { User } from "@clerk/nextjs/server";
+import { Button } from "@/components/ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import Image from "next/image";
+import { useTransition, useState } from "react";
+import LoaderIcon from "@/components/loader-icon";
 
 export default function Select({ users, userId }: { users: User[], userId: string }) {
     const searchParams = useSearchParams();
     const pathName = usePathname();
     const { replace } = useRouter();
     const [pending, startTransiton] = useTransition();
+    const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
 
-    const handleSearchParms = async (userId: string) => {
+    const handleSelect = (userId: string) => {
+        if (selectedUserId) {
+            handleSearchParms();
+        } else {
+            setSelectedUserId(userId);
+            handleSearchParms();
+        }
+    }
+
+    const handleSearchParms = async () => {
         const params = new URLSearchParams(searchParams);
+        
         await startTransiton(async () => {
             await params.set('challengeeId', userId);
             await replace(`${pathName}?${params.toString()}`);
-        })
-    }
+        });
+        
+        
+    };
 
-    const filteredUsers = users.filter(user => user.id != userId)
+    const filteredUsers = users.filter(user => user.id !== userId);
 
     return (
         <div className="grid grid-cols-1 w-full md:grid-cols-3 gap-2">
@@ -32,11 +45,16 @@ export default function Select({ users, userId }: { users: User[], userId: strin
                         <Image className="rounded-full" width={30} height={30} src={user.imageUrl} alt={"Profile image"} />
                         <CardTitle>{user.firstName} {user.lastName}</CardTitle>
                     </div>
-                    <Button disabled={pending} size="sm" variant="outline" onClick={() => handleSearchParms(user.id)}>
-                        {pending? <><LoaderIcon />Selecting</>: 'Select'}
+                    <Button 
+                        disabled={pending && selectedUserId === user.id} 
+                        size="sm" 
+                        variant="outline" 
+                        onClick={() => handleSelect(user.id)}
+                    >
+                        {pending && selectedUserId === user.id ? <><LoaderIcon /> Selecting</> : 'Select'}
                     </Button>
                 </Card>
             ))}
         </div>
-    )
+    );
 }
