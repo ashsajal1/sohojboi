2da9d5d862439c47a2471950fb177fc1e2ca74a3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-16 11:15:33 +0600

    ✨ feat: create accept btn

---
+++ src/app/challenge/accept-btn.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import LoaderIcon from '@/components/loader-icon'
+import { Button } from '@/components/ui/button'
+import { useTransition } from 'react'
+import { declineChallange } from './actions';
+import { Competition } from '@prisma/client';
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function AcceptBtn({ competition }: { competition: Competition }) {
+    const [pending, startTransiton] = useTransition();
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    const { replace } = useRouter();
+    const handleDecline = async () => {
+        const params = new URLSearchParams(searchParams);
+        
+        await startTransiton(async () => {
+            await params.set('competitionId', competition.id);
+            await replace(`${pathName}/accept?${params.toString()}`);
+        });
+    };
+
+    return (
+        <Button onClick={handleDecline} variant="outline" disabled={pending} className='w-full' size={'sm'}>
+            {pending ? <><LoaderIcon />Accept Challenge</> : 'Accept Challenge'}
+        </Button>
+    )
+}
