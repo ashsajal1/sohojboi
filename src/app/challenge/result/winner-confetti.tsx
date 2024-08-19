d3732bb102f4a5d51c83f47f726760a39c8ef07d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-19 22:50:09 +0600

    ✨ feat: add winner confetti

---
+++ src/app/challenge/result/winner-confetti.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRef } from "react";
+
+import type { ConfettiRef } from "@/components/magicui/confetti";
+import Confetti from "@/components/magicui/confetti";
+
+export default function WinnerConfetti() {
+    const confettiRef = useRef<ConfettiRef>(null);
+
+    return (
+        <Confetti
+            ref={confettiRef}
+            className="absolute left-0 top-0 z-0 size-full"
+            onLoad={() => {
+                confettiRef.current?.fire({});
+            }}
+        />
+    )
+}
