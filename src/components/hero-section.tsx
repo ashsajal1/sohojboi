40e380ac119d5972050cbd80c62561ae0a0eeadd
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-19 20:52:28 +0600

    ✨ feat: add Retro Grid

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
-import { MixIcon, QuestionMarkIcon, ShuffleIcon } from '@radix-ui/react-icons'
+import { MixIcon, QuestionMarkIcon, ShuffleIcon } from '@radix-ui/react-icons';
+import RetroGrid from "@/components/magicui/retro-grid";
 
 export default function HeroSection() {
     return (
-        <div className='flex flex-col items-center gap-4 p-6 md:p-24'>
+        <div className='flex flex-col relative items-center gap-4 p-6 md:p-24'>
             <h1 className='font-extrabold text-center text-xl md:text-3xl p-4'>Ask question and solve doubt, answer question to practice. Play quiz to enjoy. It&apos;s all about fun!</h1>
 
             <div className='flex flex-col gap-2'>
@@ -31,6 +32,8 @@ export default function HeroSection() {
 
                 </Link>
             </div>
+
+            <RetroGrid />
         </div>
     )
 }
