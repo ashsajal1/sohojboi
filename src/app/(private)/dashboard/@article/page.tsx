084369cbd115081222c585174509144306c9950d
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-24 17:59:25 +0600

    ✨ feat: create slot page

---
+++ src/app/(private)/dashboard/@article/page.tsx
@@ -0,0 +1,26 @@
+import prisma from '@/lib/prisma'
+import React from 'react'
+import { DataTable } from './data-table';
+import { columns } from './columns';
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function QuestionsSlot() {
+  let user, articles;
+  try {
+    user = await currentUser();
+    articles = await prisma.article.findMany({
+      where: {
+        authorId: user?.id!
+      }
+    });
+  } catch (err) {
+    throw err;
+  }
+
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className='font-bold text-lg'>Article List</h1>
+      <DataTable columns={columns} data={articles} />
+    </div>
+  )
+}
---
+++ src/app/(private)/dashboard/@questions/page.tsx
@@ -0,0 +1,25 @@
+import prisma from '@/lib/prisma'
+import React from 'react'
+import { DataTable } from './data-table';
+import { columns } from './columns';
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function QuestionsSlot() {
+  let user, questions;
+  try {
+    user = await currentUser();
+    questions = await prisma.question.findMany({
+      where: {
+        userId: user?.id!,
+        deletedAt: null,
+      }
+    });
+  } catch (err) {
+    throw err;
+  }
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={questions!} />
+    </div>
+  )
+}
