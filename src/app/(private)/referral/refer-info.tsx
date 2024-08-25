2ec9e5c1147264b5c703a0d121c72f5c4635103b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-25 15:32:38 +0600

    ✨ feat: add share btn

--- src/app/(private)/referral/refer-info.tsx
+++ src/app/(private)/referral/refer-info.tsx
@@ -1,4 +1,5 @@
 "use client"
+import ShareBtn from '@/components/share-btn'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card'
 import { User } from '@clerk/nextjs/server'
@@ -15,7 +16,7 @@ export default function ReferInfo({ user, refer }: { user: User, refer: Refer[] 
                 <p>Your referal count: <code>{refer.length}</code></p>
             </CardContent>
             <CardFooter>
-                <Button variant={'outline'}>Share</Button>
+                <ShareBtn title={`Use my refer code: ${user?.id} and get 100 points bonus.`} description={`Use my refer code: ${user?.id} and get 100 points bonus.`} />
             </CardFooter>
         </Card>
     )
