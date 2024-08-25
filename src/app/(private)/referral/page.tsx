2b5b33083dffb3647a87a1a10257daa03c088e50
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-25 12:41:57 +0600

    ✨ feat: check eligiblity

--- src/app/(private)/referral/page.tsx
+++ src/app/(private)/referral/page.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function ReferralPage() {
+export default async function ReferralPage() {
+    let currentActiveUser, refer, isEligible;
+
+    try {
+        currentActiveUser = await currentUser();
+        refer = await prisma.refer.findFirst({ where: { refereeId: currentActiveUser?.id } });
+
+        isEligible = !!refer?.refereeId;
+    } catch (error) {
+        throw error;
+    }
+
     return (
         <>
-            <div>
-                <form className='flex flex-col gap-2'>
+            <div className="p-4">
+                {!isEligible ? <form className='flex flex-col gap-2'>
                     <Input placeholder='Enter referral code..' />
                     <Button type="submit">Submit</Button>
-                </form>
+                </form> :
+                    <p>Sorry, You are not eligible.</p>
+                }
             </div>
         </>
     )
