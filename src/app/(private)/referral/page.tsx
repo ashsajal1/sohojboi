e762873533ce7217e1b56447c8ece5b6018caec3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-25 14:41:20 +0600

    ✨ feat: add refer info

--- src/app/(private)/referral/page.tsx
+++ src/app/(private)/referral/page.tsx
@@ -1,15 +1,16 @@
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import ReferForm from "./refer-form";
+import ReferInfo from "./refer-info";
 
 export default async function ReferralPage() {
     let currentActiveUser, refer, isEligible;
 
     try {
         currentActiveUser = await currentUser();
-        refer = await prisma.refer.findFirst({ where: { refereeId: currentActiveUser?.id } });
+        refer = await prisma.refer.findMany({ where: { refereeId: currentActiveUser?.id } });
 
-        isEligible = !!refer?.refereeId;
+        isEligible = refer.length > 0;
     } catch (error) {
         throw error;
     }
@@ -20,6 +21,8 @@ export default async function ReferralPage() {
                 {!isEligible ? <ReferForm /> :
                     <p>Sorry, You are not eligible.</p>
                 }
+
+               <ReferInfo refer={refer} user={JSON.parse(JSON.stringify(currentActiveUser!))} />
             </div>
         </>
     )
