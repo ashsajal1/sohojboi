42eb247e538a838700db664850dfa4784e77c826
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-25 14:03:40 +0600

    ✨ feat: handle invalid id

--- src/app/(private)/referral/actions.tsx
+++ src/app/(private)/referral/actions.tsx
@@ -1,35 +1,40 @@
 "use server"
 
 import prisma from "@/lib/prisma";
-import { currentUser } from "@clerk/nextjs/server";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
-
 export const claimReferBonus = async (refererId: string) => {
     try {
         const user = await currentUser();
-        await prisma.profile.upsert({
-            where: {
-                clerkUserId: refererId,
-            },
-            create: {
-                clerkUserId: refererId,
-                rewardCount: 100,
-                bio: "default",
-            },
-            update: {
-                rewardCount: {
-                    increment: 100,
-                },
-            },
-        });
+        const refererUser = await clerkClient().users.getUser(refererId)
 
-        await prisma.refer.create({
-            data: {
-                refereeId: user?.id!,
-                referrerId: refererId,
-                referredAt: new Date(),
-            }
-        })
+        if (refererUser) {
+            await prisma.profile.upsert({
+                where: {
+                    clerkUserId: refererId,
+                },
+                create: {
+                    clerkUserId: refererId,
+                    rewardCount: 100,
+                    bio: "default",
+                },
+                update: {
+                    rewardCount: {
+                        increment: 100,
+                    },
+                },
+            });
+
+            await prisma.refer.create({
+                data: {
+                    refereeId: user?.id!,
+                    referrerId: refererId,
+                    referredAt: new Date(),
+                }
+            })
+        } else {
+            throw new Error("User not found");
+        }
 
         revalidatePath("/");
     } catch (error) {
