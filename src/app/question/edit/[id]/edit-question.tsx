41c8af7caa912e30141594bb6f23cf76ac7433bc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-26 21:48:25 +0600

    ✨ feat: replace select

--- src/app/question/edit/[id]/edit-question.tsx
+++ src/app/question/edit/[id]/edit-question.tsx
@@ -1,28 +1,33 @@
 "use client";
 
-import { useTransition } from "react";
+import { useTransition, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Question, Topic } from "@prisma/client";
 import { editQuestion } from "./actions";
 import {
-  Select,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-  SelectContent,
-  SelectGroup,
-  SelectLabel,
-} from "@/components/ui/select";
+  PopoverTrigger,
+  Popover,
+  PopoverContent,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { ArrowUpIcon, CheckIcon } from "@radix-ui/react-icons";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
-import { TrashIcon } from "@radix-ui/react-icons";
 import DeleteQuestion from "./delete-question";
+import { cn } from "@/lib/utils";
 
 const EditQuestion = ({ question, topics }: { question: Question; topics: Topic[] }) => {
   const [pending, startTransition] = useTransition();
+  const [open, setOpen] = useState(false);
 
   const questionSchema = z.object({
     title: z
@@ -69,25 +74,59 @@ const EditQuestion = ({ question, topics }: { question: Question; topics: Topic[
         <div className="space-y-4">
           <div className="space-y-2">
             <h2 className="text-lg font-medium">Topic</h2>
-            <Select
-              disabled={pending}
-              onValueChange={(newValue) => formMethods.setValue("topicId", newValue)}
-              defaultValue={question.topicId!}
-            >
-              <SelectTrigger>
-                <SelectValue placeholder="Select a topic"></SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  <SelectLabel>Topic</SelectLabel>
-                  {topics.map((topic) => (
-                    <SelectItem value={topic.id} key={topic.id}>
-                      {topic.name}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
+            <Controller
+              control={formMethods.control}
+              name="topicId"
+              render={({ field }) => (
+                <Popover open={open} onOpenChange={setOpen}>
+                  <PopoverTrigger className="w-full" asChild>
+                    <Button
+                      disabled={pending}
+                      variant="outline"
+                      role="combobox"
+                      aria-expanded={open}
+                      className="w-full justify-between"
+                      onClick={() => setOpen(true)}
+                    >
+                      {field.value
+                        ? topics.find((topic) => topic.id === field.value)?.name
+                        : "Select topic..."}
+                      <ArrowUpIcon className="ml-2 -rotate-180 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                      <CommandInput placeholder="Search topic..." />
+                      <CommandEmpty>No topic found.</CommandEmpty>
+                      <CommandList>
+                        {Array.isArray(topics) && topics.length > 0 ? (
+                          topics.map((topic) => (
+                            <CommandItem
+                              key={topic.id}
+                              value={topic.name}
+                              onSelect={(currentValue) => {
+                                field.onChange(topics.find((t) => t.name === currentValue)?.id);
+                                setOpen(false);
+                              }}
+                            >
+                              <CheckIcon
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  field.value === topic.id ? "opacity-100" : "opacity-0"
+                                )}
+                              />
+                              {topic.name}
+                            </CommandItem>
+                          ))
+                        ) : (
+                          <CommandEmpty>No topics available</CommandEmpty>
+                        )}
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              )}
+            />
             {formMethods.formState.errors.topicId && (
               <p className="text-sm text-red-500">{formMethods.formState.errors.topicId.message}</p>
             )}
