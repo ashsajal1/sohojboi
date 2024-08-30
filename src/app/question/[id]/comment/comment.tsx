510a071ca463ff991ad776a8de5a30f6e2f80c7b
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-30 12:04:04 +0600

    ✨ feat: create edit form

--- src/app/question/[id]/comment/comment.tsx
+++ src/app/question/[id]/comment/comment.tsx
@@ -1,11 +1,12 @@
 import { Separator } from '@/components/ui/separator'
+import EditComment from './edit-comment'
 export default function Comment() {
     return (
         <div className="text-sm text-muted-foreground/60">
             <div className="flex items-center gap-2">
                 <p>This is my comment</p>
                 <p className="font-semibold">- Ashfiquzzaman Sajal</p>
-                <p className="text-blue-600 cursor-pointer">Edit</p>
+                <EditComment />
                 <p className="text-red-600 cursor-pointer">Delete</p>
             </div>
             <Separator className="mt-2 w-full" />
