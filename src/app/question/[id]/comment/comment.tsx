e645b2bc822f835243d804aaef14f059cfa5b527
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-31 07:03:59 +0600

    ✨ feat: add props

--- src/app/question/[id]/comment/comment.tsx
+++ src/app/question/[id]/comment/comment.tsx
@@ -3,12 +3,13 @@
 import { Separator } from '@/components/ui/separator'
 import EditComment from './edit-comment'
 import DeleteComment from './delete-comment'
-export default function Comment() {
+import { AnswerComment } from '@prisma/client'
+export default function Comment({comment}:{comment: AnswerComment}) {
     
     return (
         <div className="text-sm text-muted-foreground/60 py-3">
             <div className="flex items-center gap-2">
-                <p>This is my comment</p>
+                <p>{comment.content}</p>
                 <p className="font-semibold">- Ashfiquzzaman Sajal</p>
                 <EditComment />
                 <DeleteComment />
