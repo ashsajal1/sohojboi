930bb6e828e190d719d7819705e561d2f772b9ec
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-31 09:43:20 +0600

    ✨ feat: show user name

--- src/app/question/[id]/comment/comment.tsx
+++ src/app/question/[id]/comment/comment.tsx
@@ -4,13 +4,26 @@ import { Separator } from '@/components/ui/separator'
 import EditComment from './edit-comment'
 import DeleteComment from './delete-comment'
 import { AnswerComment } from '@prisma/client'
-export default function Comment({comment}:{comment: AnswerComment}) {
-    
+import { useEffect, useState } from 'react'
+import { getName } from './actions'
+
+export default function Comment({ comment }: { comment: AnswerComment }) {
+    const [name, setName] = useState<any>('');
+
+    useEffect(() => {
+        const fetchName = async () => {
+            const fullName = await getName(comment.userId);
+            setName(fullName);
+        };
+
+        fetchName();
+    }, [comment.userId]);
+
     return (
         <div className="text-sm text-muted-foreground/60 py-3">
             <div className="flex items-center gap-2">
                 <p>{comment.content}</p>
-                <p className="font-semibold">- Ashfiquzzaman Sajal</p>
+                <p className="font-semibold">- {name}</p>
                 <EditComment />
                 <DeleteComment />
             </div>
