89ebc856dbb875dc462ce45ab4b6b09460e358ac
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-31 16:29:04 +0600

    🦄 refactor: remove unused

--- src/app/question/[id]/comment/edit-comment.tsx
+++ src/app/question/[id]/comment/edit-comment.tsx
@@ -10,8 +10,6 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { Input } from '@/components/ui/input';
 
 export default function EditComment() {
     return (
