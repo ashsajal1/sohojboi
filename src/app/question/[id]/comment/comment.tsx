124d77f08c6ddcf211b3e5dae39a7f85af11ca0c
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-31 06:14:52 +0600

    ✨ feat: add client tag

--- src/app/question/[id]/comment/comment.tsx
+++ src/app/question/[id]/comment/comment.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import { Separator } from '@/components/ui/separator'
 import EditComment from './edit-comment'
 import DeleteComment from './delete-comment'
 export default function Comment() {
+    
     return (
-        <div className="text-sm text-muted-foreground/60">
+        <div className="text-sm text-muted-foreground/60 py-3">
             <div className="flex items-center gap-2">
                 <p>This is my comment</p>
                 <p className="font-semibold">- Ashfiquzzaman Sajal</p>
