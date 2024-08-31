69e2c8231a2446a7dfca3b8913739888f1579489
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-31 07:03:40 +0600

    ✨ feat: create action

--- src/app/question/[id]/comment/actions.ts
+++ src/app/question/[id]/comment/actions.ts
@@ -1,13 +1,25 @@
 "use server";
 
+import prisma from "@/lib/prisma";
 import { CommentFormData, commentSchema } from "./type";
+import { auth } from "@clerk/nextjs/server";
 
-export const createComment = async (data: CommentFormData) => {
+export const createComment = async (data: CommentFormData, answerId: string) => {
   // Validate the data using Zod
   try {
+    const user = await auth();
+    if(user.userId === null) {
+        throw new Error("Unauthorized access");
+    }
     const validatedData = commentSchema.parse(data);
     // Proceed with the validated data (e.g., saving to the database)
-    console.log(validatedData);
+    await prisma.answerComment.create({
+      data: {
+        content: validatedData.content,
+        answerId: answerId,
+        userId: user.userId!,
+      },
+    })
   } catch (error) {
     return error;
   }
