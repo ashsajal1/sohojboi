220e71bc92800173d1c70a5a65322f680cf05d13
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-09-01 08:31:44 +0600

    ✨ feat: highlight solution

--- src/app/question/[id]/answers.tsx
+++ src/app/question/[id]/answers.tsx
@@ -20,6 +20,10 @@ import Comment from "./comment/comment";
 import CommentSection from "./comment/comment-section";
 import { BookOpenCheck } from "lucide-react";
 import MarkSolution from "./mark-solution";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import AnimatedShinyText from "@/components/magicui/animated-shiny-text";
 
 interface AnswersProps {
     answers: Answer[];
@@ -57,6 +61,18 @@ const Answer = async ({ answer, question }: { answer: Answer, question: Question
 
     return <Card>
         <CardHeader>
+            {answer.isSolution && <div className="z-10 flex items-center justify-start">
+                <div
+                    className={cn(
+                        "group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800",
+                    )}
+                >
+                    <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
+                        <span>✅ Solution</span>
+                        
+                    </AnimatedShinyText>
+                </div>
+            </div>}
             <div className="flex justify-between items-start">
                 <CardDescription>
                     <ReactMarkdown>{answer.answer}</ReactMarkdown>
