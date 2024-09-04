56df6252714c004a04d95487b863dc420f8590cb
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-09-04 12:35:45 +0600

    🐞fix: user id

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -83,7 +83,7 @@ export default async function Page({ searchParams }: { searchParams: { id: strin
 
     const profile = await prisma.profile.findFirst({
         where: {
-            clerkUserId: searchParams.id as string,
+            clerkUserId: user?.id,
         }
     })
 
