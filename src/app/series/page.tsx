710c1ed4b3a4ebe12511671282090b66c1974c31
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-10-04 11:57:35 +0600

    feat: link  series id

--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma"
 import { auth } from "@clerk/nextjs/server"
 import SeriesForm from "./form"
+import Link from "next/link"
 
 export default async function SeriesPage() {
     const user = auth()
@@ -15,7 +16,7 @@ export default async function SeriesPage() {
             {series.length === 0 && <p>No Series Found!</p>}
 
             {series.map((series) => (
-                <p key={series.id}>{series.title}</p>
+                <Link href={`/series/${series.id}`} key={series.id}>{series.title}</Link>
             ))}
 
             <h1 className="font-bold text-lg">Create series</h1>
