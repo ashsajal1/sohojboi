d057478069bf2c67861db4fa1bc8aaf54a9d9f42
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-10-04 12:11:03 +0600

    feat: add edit btn

--- src/app/series/[id]/page.tsx
+++ src/app/series/[id]/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server";
+import { EditIcon } from "lucide-react";
 import Link from "next/link"
 
 export default async function Series({ params }: { params: { id: string } }) {
@@ -11,14 +14,23 @@ export default async function Series({ params }: { params: { id: string } }) {
         }
     })
 
+    const user = await auth();
+    const isAuthor = user.userId === series?.userId;
+
     return (
         <div>
-            <h1 className="font-bold text-lg">{series?.title}</h1>
+            <div className="flex justify-between items-center">
+                <h1 className="font-bold text-lg">{series?.title}</h1>
+                {isAuthor && <Button size='icon' variant={'ghost'}>
+                    <EditIcon className="h-5 w-5" />
+                </Button>}
+            </div>
             <p className="text-sm text-muted-foreground/70">{series?.description}</p>
 
             {series?.articles.map((article) => (
                 <Link href={`/article/${article.id}`} key={article.id}>{article.title}</Link>
             ))}
+            {isAuthor && <Button>Add Article</Button>}
         </div>
     )
 }
