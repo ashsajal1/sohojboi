4796e1364548f3d43dab744ec9c4080acaec2a51
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-10-04 21:37:37 +0600

    style: layout

--- src/app/series/[id]/page.tsx
+++ src/app/series/[id]/page.tsx
@@ -4,6 +4,10 @@ import { auth } from "@clerk/nextjs/server";
 import { EditIcon } from "lucide-react";
 import Link from "next/link"
 import AddArticle from "./add-article";
+import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import Markdown from 'react-markdown'
+import ProfileImgCard from '@/components/profile-img-card'
+import Views from "@/app/article/[id]/views";
 
 export default async function Series({ params }: { params: { id: string } }) {
     const series = await prisma.blogSeries.findUnique({
@@ -28,9 +32,30 @@ export default async function Series({ params }: { params: { id: string } }) {
             </div>
             <p className="text-sm text-muted-foreground/70">{series?.description}</p>
 
+            <div className='grid md:grid-cols-2 gap-2 py-2'>           
             {series?.articles.map((article) => (
-                <Link href={`/article/${article.id}`} key={article.id}>{article.title}</Link>
+                <Card key={article.id}>
+                <CardHeader>
+                    <CardTitle>{article.title}</CardTitle>
+                    <Markdown className={'text-sm text-muted-foreground'}>
+                        {article.content.replace(/\n/g, ' ').slice(0, 260)}
+                    </Markdown>
+                </CardHeader>
+                <CardFooter>
+                    <div className='flex items-center justify-between w-full'>
+                        <ProfileImgCard type={'article'} userId={article.authorId} createdAt={article.createdAt} />
+                        <div className='flex justify-end items-center gap-2'>
+                            <Views articleId={article.id} />
+                            <Link href={`/article/${article.id}`}>
+                                <Button variant={'ghost'}>Read more</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </CardFooter>
+            </Card>
             ))}
+            </div>
+
             {isAuthor && <AddArticle userId={user.userId} seriesId={params.id} />}
         </div>
     )
