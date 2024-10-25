964aa445a1b094cda48bb04713f0c90b1c4df9f3
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-10-25 22:43:52 +0600

    feat: trigger check in
    Check checked-in in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import HeroSection from "@/components/hero-section";
+import { currentUser } from "@clerk/nextjs/server";
+import doCheckIn from "./_actions/doCheckIn";
+import CheckInConfirmationDialog from "@/components/check-in";
 
 export default async function Home() {
+  const user = await currentUser();
+  let checkInStatus;
+  if (user) {
+    checkInStatus = await doCheckIn(user.id);
+    console.log("Check-in status:", checkInStatus);
+    if(checkInStatus) {
+      console.log("Checked in added bonus.")
+    }
+  }
   return (
     <>
+    {checkInStatus && <CheckInConfirmationDialog />}
       <HeroSection />
     </>
 
