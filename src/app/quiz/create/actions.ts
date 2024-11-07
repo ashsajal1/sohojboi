2c6a83d23278997db78056ebf2f11f7c2cbdcebc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-07 11:36:53 +0600

    feat: prevent unauthorized

--- src/app/quiz/create/actions.ts
+++ src/app/quiz/create/actions.ts
@@ -2,9 +2,14 @@
 import prisma from "@/lib/prisma";
 import { QuestionFormData } from "./create-form";
 import { revalidatePath } from "next/cache";
+import { currentUser } from "@clerk/nextjs/server";
 
 export const createChallengeQuestion = async (formData: QuestionFormData) => {
   try {
+    const user = await currentUser();
+    if (!user) {
+      throw new Error("User is not authorized!");
+    }
     const options = formData.options.map((option) => {
       return {
         content: option.content,
