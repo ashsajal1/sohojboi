887f150965fe421d8c61b4855a16acccb1f24995
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-10 19:58:41 +0600

    feat: add article section

--- src/app/quiz/create/actions.ts
+++ src/app/quiz/create/actions.ts
@@ -10,26 +10,34 @@ export const createChallengeQuestion = async (formData: QuestionFormData) => {
     if (!user) {
       throw new Error("User is not authorized!");
     }
-    const options = formData.options.map((option) => {
-      return {
-        content: option.content,
-        isCorrect: false,
-      };
-    });
-    const question = await prisma.challengeQuestion.create({
-      data: {
-        content: formData.content,
-        topic: { connect: { id: formData.topic } },
-        article: { connect: { id: formData.article } },
-        deletedAt: null,
-        options: {
-          create: [
-            { content: formData.correctOption, isCorrect: true },
-            ...options,
-          ],
-        },
+
+    const options = formData.options.map((option) => ({
+      content: option.content,
+      isCorrect: false,
+    }));
+
+    const questionData: any = {
+      content: formData.content,
+      topic: { connect: { id: formData.topic } },
+      article: { connect: { id: formData.article } },
+      deletedAt: null,
+      options: {
+        create: [
+          { content: formData.correctOption, isCorrect: true },
+          ...options,
+        ],
       },
+    };
+
+    // Conditionally add articleSection connection if it exists
+    if (formData.articleSection) {
+      questionData.articleSection = { connect: { id: formData.articleSection } };
+    }
+
+    const question = await prisma.challengeQuestion.create({
+      data: questionData,
     });
+
   } catch (error) {
     return error;
   }
