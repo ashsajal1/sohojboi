5fca30e235e8b52281048068148b7084c4cc50bc
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-11 10:34:22 +0600

    feat: create actions for  many quiz

--- src/app/quiz/create/actions.ts
+++ src/app/quiz/create/actions.ts
@@ -31,16 +31,68 @@ export const createChallengeQuestion = async (formData: QuestionFormData) => {
 
     // Conditionally add articleSection connection if it exists
     if (formData.articleSection) {
-      questionData.articleSection = { connect: { id: formData.articleSection } };
+      questionData.articleSection = {
+        connect: { id: formData.articleSection },
+      };
     }
 
     const question = await prisma.challengeQuestion.create({
       data: questionData,
     });
-
   } catch (error) {
     return error;
   }
 
   revalidatePath("/");
+};
+
+export const createManyQuestions = async (
+  topic: string,
+  questions: {
+    content: string;
+    options: string[];
+    correctOption: string;
+    hint: string | null;
+    explanation: string | null;
+  }[],
+  article?: string
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      throw new Error("User is not authorized!");
+    }
+
+    // Iterate and create each question individually
+    for (const question of questions) {
+      const options = [
+        { content: question.correctOption, isCorrect: true },
+        ...question.options.map((opt) => ({ content: opt, isCorrect: false })),
+      ];
+
+      // Define question data with conditional article connection
+      const questionData: any = {
+        content: question.content,
+        options: { create: options },
+        hint: question.hint,
+        explanation: question.explanation,
+        topic: { connect: { id: topic } },
+      };
+
+      if (article) {
+        questionData.article = { connect: { id: article } };
+      }
+
+      // Create each question individually
+      await prisma.challengeQuestion.create({
+        data: questionData,
+      });
+    }
+
+    // Optionally revalidate cache for updated data
+    revalidatePath("/");
+  } catch (error) {
+    console.error("Error creating questions:", error);
+    return error;
+  }
 };
