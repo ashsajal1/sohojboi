a9d751732fb6f2136658ba114869d89700663036
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-17 19:45:09 +0600

    style: change confetti

--- src/components/check-in.tsx
+++ src/components/check-in.tsx
@@ -19,33 +19,41 @@ function CheckInConfirmationDialog() {
     const [open, setOpen] = useState(true)
 
     const triggerConfetti = () => {
-        const duration = 5 * 1000;
-        const animationEnd = Date.now() + duration;
-        const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-        const randomInRange = (min: number, max: number) =>
-            Math.random() * (max - min) + min;
-
-        const interval = window.setInterval(() => {
-            const timeLeft = animationEnd - Date.now();
-
-            if (timeLeft <= 0) {
-                return clearInterval(interval);
-            }
-
-            const particleCount = 50 * (timeLeft / duration);
-            confetti({
-                ...defaults,
-                particleCount,
-                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-            });
-            confetti({
-                ...defaults,
-                particleCount,
-                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-            });
-        }, 250);
-    };
+        const scalar = 2;
+        const unicorn = confetti.shapeFromText({ text: "💖", scalar });
+     
+        const defaults = {
+          spread: 360,
+          ticks: 60,
+          gravity: 0,
+          decay: 0.96,
+          startVelocity: 20,
+          shapes: [unicorn],
+          scalar,
+        };
+     
+        const shoot = () => {
+          confetti({
+            ...defaults,
+            particleCount: 30,
+          });
+     
+          confetti({
+            ...defaults,
+            particleCount: 5,
+          });
+     
+          confetti({
+            ...defaults,
+            particleCount: 15,
+            scalar: scalar / 2,
+            shapes: ["circle"],
+          });
+        };
+     
+        setTimeout(shoot, 0);
+        setTimeout(shoot, 100);
+      };
 
     const doCongrass = () => {
         triggerConfetti();
