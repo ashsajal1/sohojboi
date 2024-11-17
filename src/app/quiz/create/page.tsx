ec50b92e20e3346253a29eadc89751ae8ea17ea4
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-11-17 07:36:37 +0600

    feat: add tabs

--- src/app/quiz/create/page.tsx
+++ src/app/quiz/create/page.tsx
@@ -1,6 +1,12 @@
 import prisma from "@/lib/prisma";
 import CreateForm from "./create-form";
-
+import JsonInput from "./json-input";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
 export default async function page() {
   const topics = await prisma.topic.findMany();
   const articles = await prisma.article.findMany({
@@ -19,7 +25,20 @@ export default async function page() {
 
   return (
     <div>
-      <CreateForm topics={topics} articles={articles} articleSections={articleSections} />
+      <Tabs defaultValue="manual" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="manual">Manual</TabsTrigger>
+          <TabsTrigger value="json-input">Json Input</TabsTrigger>
+        </TabsList>
+        <TabsContent value="manual">
+          <CreateForm topics={topics} articles={articles} articleSections={articleSections} />
+        </TabsContent>
+        <TabsContent value="json-input">
+          <JsonInput topics={topics} articles={articles} articleSections={articleSections} />
+        </TabsContent>
+      </Tabs>
+
+
     </div>
   );
 }
