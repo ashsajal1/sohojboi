05e9acfb8355098b2288514df148ef3dd36c8e13
Ashsajal <ashsajal1@gmail.com>
2025-01-25 23:04:42 +0600

    refactor: code

--- src/app/challenge/challange.tsx
+++ src/app/challenge/challange.tsx
@@ -1,139 +1,172 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { createCompetition, getChallengeData } from './actions';
-import { AnswerOption, ChallengeQuestion } from '@prisma/client';
-import { User } from '@clerk/nextjs/server';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
-import { Separator } from '@/components/ui/separator';
-import { Progress } from '@/components/ui/progress';
-import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { createCompetition, getChallengeData } from "./actions";
+import { AnswerOption, ChallengeQuestion } from "@prisma/client";
+import { User } from "@clerk/nextjs/server";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
+import { Progress } from "@/components/ui/progress";
+import { useRouter } from "next/navigation";
 
 interface ChallengeProps {
-    topic: string;
-    challengeeId: string;
-    challenger: User;
-    quizId: string;
-    quizQuestions: (ChallengeQuestion & { options: AnswerOption[] })[];
+  topic: string;
+  challengeeId: string;
+  challenger: User;
+  quizId: string;
+  quizQuestions: (ChallengeQuestion & { options: AnswerOption[] })[];
 }
 
-const Challenge: React.FC<ChallengeProps> = ({ challengeeId, challenger, quizId,topic, quizQuestions }) => {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const [selectedOption, setSelectedOption] = useState<string | null>(null);
-    const [score, setScore] = useState(0);
-    const [showResults, setShowResults] = useState(false);
-    const [challengee, setChallenge] =  useState<User | null>()
-    const router = useRouter()
+const Challenge: React.FC<ChallengeProps> = ({
+  challengeeId,
+  challenger,
+  quizId,
+  topic,
+  quizQuestions,
+}) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const [score, setScore] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const [challengee, setChallenge] = useState<User | null>();
+  const router = useRouter();
 
-    const questionsIds = quizQuestions.map(q => q.id);
+  const questionsIds = quizQuestions.map((q) => q.id);
 
-    useEffect(() => {
-        const fetchData = async () => {
-             let data = await getChallengeData(challengeeId)
-             setChallenge(data)
-          }
-        fetchData();
-        
-    },[challengeeId])
-
-    useEffect(() => {
-        createCompetitionFunc();
-
-        async function createCompetitionFunc() {
-            if (showResults) {
-                const competition =await createCompetition(challengeeId, challenger.id, questionsIds, score);
-                router.push(`/challenge/result?competitionId=${competition.id}`)
-            }
-        }
-    }, [challengeeId, challenger, questionsIds, router, score, showResults])
-
-    const handleOptionSelect = (optionId: string) => {
-        setSelectedOption(optionId);
+  useEffect(() => {
+    const fetchData = async () => {
+      let data = await getChallengeData(challengeeId);
+      setChallenge(data);
     };
+    fetchData();
+  }, [challengeeId]);
 
-    const nextQuestion = async () => {
-        const currentQuestion = quizQuestions[currentQuestionIndex];
-        const selectedOptionObject = currentQuestion.options.find((option) => option.id === selectedOption);
+  useEffect(() => {
+    if (showResults) {
+      createCompetitionFunc();
+    }
 
-        if (selectedOptionObject?.isCorrect) {
-            setScore(score + 1);
-        }
+    async function createCompetitionFunc() {
+      const competition = await createCompetition(
+        challengeeId,
+        challenger.id,
+        questionsIds,
+        score
+      );
+      router.push(`/challenge/result?competitionId=${competition.id}`);
+    }
+  }, [challengeeId, challenger, questionsIds, router, score, showResults]);
 
-        if (currentQuestionIndex < quizQuestions.length - 1) {
-            setCurrentQuestionIndex(currentQuestionIndex + 1);
-            setSelectedOption(null);
-        } else {
-            setShowResults(true);
-        }
-    };
+  const handleOptionSelect = (optionId: string) => {
+    setSelectedOption(optionId);
+  };
 
-    return (
-        <div className='w-full'>
-            {quizQuestions.length > 0 && (
-                <>
-                    {showResults ? (
-                        <Card>
-                            <CardHeader>
-                                <CardTitle>Results:</CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                <p>You scored {score} out of {quizQuestions.length}</p>
-                            </CardContent>
-                        </Card>
-                    ) : (
-                        <Card>
-                            <CardHeader>
-                                <div className='flex justify-between py-2'>
-                                <Avatar className=''>
-                                    <AvatarImage src={challenger?.imageUrl} />
-                                    <AvatarFallback>{challenger?.firstName?.slice(0, 1)}</AvatarFallback>
-                                </Avatar>
-                                <p>Topic : {topic}</p>
-
-                                <Avatar className=''>
-                                    <AvatarImage src={challengee?.imageUrl} />
-                                    <AvatarFallback>{challengee?.firstName?.slice(0, 1)}</AvatarFallback>
-                                </Avatar>
-                                </div>
-
-                                <Progress className='mt-2' value={((currentQuestionIndex + 1) / quizQuestions.length) * 100} />
-
-                                <CardTitle className='py-2'><p>
-                                {currentQuestionIndex + 1}. {quizQuestions[currentQuestionIndex].content}</p></CardTitle>
-                            </CardHeader>
-
-                            
-                            <CardContent>
-                                <ul>
-                                    {quizQuestions[currentQuestionIndex].options.map((option) => (
-                                        <li key={option.id}>
-                                            <Button
-                                                variant={'outline'}
-                                                className='w-full mb-2'
-                                                onClick={() => handleOptionSelect(option.id)}
-                                                disabled={selectedOption !== null}
-                                            >
-                                                {option.content}
-                                            </Button>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </CardContent>
-
-                            
-                            <CardFooter>
-                                {selectedOption !== null && (
-                                    <Button className='w-full' onClick={nextQuestion}>Next</Button>
-                                )}
-                            </CardFooter>
-                        </Card>
-                    )}
-                </>
-            )}
-        </div>
+  const nextQuestion = async () => {
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+    const selectedOptionObject = currentQuestion.options.find(
+      (option) => option.id === selectedOption
     );
+
+    if (selectedOptionObject?.isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestionIndex < quizQuestions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelectedOption(null);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  return (
+    <div className="w-full">
+      {quizQuestions.length > 0 && (
+        <>
+          {showResults ? (
+            <Card>
+              <CardHeader>
+                <CardTitle>Results:</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>
+                  You scored {score} out of {quizQuestions.length}
+                </p>
+              </CardContent>
+            </Card>
+          ) : (
+            <Card>
+              <CardHeader>
+                <div className="flex justify-between py-2">
+                  <Avatar className="">
+                    <AvatarImage src={challenger?.imageUrl} />
+                    <AvatarFallback>
+                      {challenger?.firstName?.slice(0, 1)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <p>Topic : {topic}</p>
+
+                  <Avatar className="">
+                    <AvatarImage src={challengee?.imageUrl} />
+                    <AvatarFallback>
+                      {challengee?.firstName?.slice(0, 1)}
+                    </AvatarFallback>
+                  </Avatar>
+                </div>
+
+                <Progress
+                  className="mt-2"
+                  value={
+                    ((currentQuestionIndex + 1) / quizQuestions.length) * 100
+                  }
+                />
+
+                <CardTitle className="py-2">
+                  <p>
+                    {currentQuestionIndex + 1}.{" "}
+                    {quizQuestions[currentQuestionIndex].content}
+                  </p>
+                </CardTitle>
+              </CardHeader>
+
+              <CardContent>
+                <ul>
+                  {quizQuestions[currentQuestionIndex].options.map((option) => (
+                    <li key={option.id}>
+                      <Button
+                        variant={"outline"}
+                        className="w-full mb-2"
+                        onClick={() => handleOptionSelect(option.id)}
+                        disabled={selectedOption !== null}
+                      >
+                        {option.content}
+                      </Button>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <CardFooter>
+                {selectedOption !== null && (
+                  <Button className="w-full" onClick={nextQuestion}>
+                    Next
+                  </Button>
+                )}
+              </CardFooter>
+            </Card>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Challenge;
