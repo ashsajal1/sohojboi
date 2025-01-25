8e34f8c7cd12509281dff3408b043745cc6eb1fd
Ashsajal <ashsajal1@gmail.com>
2025-01-25 16:46:47 +0600

    feat: add katex

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,6 +3,9 @@ import MarkdownPreview from '@uiw/react-markdown-preview';
 import rehypeSanitize from "rehype-sanitize";
 const rehypePlugins = [rehypeSanitize];
 import { useTheme } from "next-themes"
+import { getCodeString } from 'rehype-rewrite';
+import katex from 'katex';
+import 'katex/dist/katex.css';
 
 export default function Content({ content }: { content: string }) {
     const { theme } = useTheme();
@@ -13,7 +16,29 @@ export default function Content({ content }: { content: string }) {
             }}
             className='p-4'
             rehypePlugins={rehypePlugins}
-            source={content}>
+            source={content}
+            components={{
+                code: ({ children = [], className, ...props }) => {
+                  if (typeof children === 'string' && /^\$\$(.*)\$\$/.test(children)) {
+                    const html = katex.renderToString(children.replace(/^\$\$(.*)\$\$/, '$1'), {
+                      throwOnError: false,
+                    });
+                    return <code dangerouslySetInnerHTML={{ __html: html }} style={{ background: 'transparent' }} />;
+                  }
+                  const code = props.node && props.node.children ? getCodeString(props.node.children) : children;
+                  if (
+                    typeof code === 'string' &&
+                    typeof className === 'string' &&
+                    /^language-katex/.test(className.toLocaleLowerCase())
+                  ) {
+                    const html = katex.renderToString(code, {
+                      throwOnError: false,
+                    });
+                    return <code style={{ fontSize: '150%' }} dangerouslySetInnerHTML={{ __html: html }} />;
+                  }
+                  return <code className={String(className)}>{children}</code>;
+                },
+              }}>
 
         </MarkdownPreview>
     )
