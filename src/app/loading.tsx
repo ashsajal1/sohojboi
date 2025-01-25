371db3fff49a31260676db7de3993a9b6989a0c7
Ashsajal <ashsajal1@gmail.com>
2025-01-25 17:28:45 +0600

    style: udate skeleton
    increase height

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,13 +4,14 @@ import React from 'react'
 export default function loading() {
     return (
         <div className='flex flex-col gap-2'>
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
-            <Skeleton className='w-full h-[100px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            <Skeleton className='w-full h-[250px]' />
+            
         </div>
     )
 }
