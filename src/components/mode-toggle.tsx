d5f425d054864dbec214f5f3b87e02e247907c3b
Ashsajal <ashsajal1@gmail.com>
2025-01-27 16:47:13 +0600

    refactor: remove imports

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -5,12 +5,6 @@ import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
